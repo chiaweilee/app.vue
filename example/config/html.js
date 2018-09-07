f87301dc7b32cb9e5d041243fb3de4c5d8876737
jv <ljw@live.jp>
2018-09-07 10:44:13 +0800

    0.1.18

--- example/config/html.js
+++ example/config/html.js
@@ -21,13 +21,12 @@ const scripts = [
 ]
 
 const meta = [
+  '<meta charset="utf-8">',
+  '<meta name="viewport" content="initial-scale=1.0">',
+  '<meta name=renderer content=webkit>',
+  '<meta name=force-rendering content=webkit>',
+  '<meta http-equiv=X-UA-Compatible content="IE=Edge, chrome=1">',
   '<meta name="viewport" content="width=device-width,initial-scale=1.0"/>',
-  '<meta name="renderer" content="webkit"/>',
-  '<meta name="force-rendering" content="webkit"/>',
-  '<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"/>',
-  '<meta name="google-site-verification" content="rHdeQjP4i-QBLNcp5t9cEjKcK1BYHnbz3vV9957YjOE"/>',
-  '<link rel="shortcut icon" href="/static/ubibi.ico"/>',
-  '<link rel="bookmark" href="/static/ubibi.ico" type="image/x-icon"/>'
 ]
 
 module.exports = {
