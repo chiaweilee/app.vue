e88303123f7a1f9407a1c3d27d3a9c466dc86a27
jv <ljw@live.jp>
2018-09-25 16:33:45 +0800

    refactor install

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 /*!
- * app-vue v0.1.2
- * (c) 2018 +v
- * @license MIT
+ * app-vue +v
  */
 import components from './components'
 
-const install = function (Vue) {
-  for (let component in components) {
-    Vue.component('app-' + component, components[component])
-  }
-}
-
 export default {
-  install
+  install (Vue) {
+    Object.keys(components).forEach(c => {
+      Vue.component(`app-${c}`, components[c])
+    })
+  }
 }
