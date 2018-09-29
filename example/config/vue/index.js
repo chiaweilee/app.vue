db066feac9fa1271df43df53c8f725ad0c3bef40
jv <ljw@live.jp>
2018-09-29 16:58:25 +0800

    refactor

--- example/config/vue/index.js
+++ example/config/vue/index.js
@@ -3,15 +3,25 @@ import components from './components'
 import filters from './filters'
 import appVue from '../../../src/index'
 import VueProgressBar from 'vue-progressbar-v'
+import use from 'vue-use'
+import VueRouter from '../vue-router'
+import Vuex from '../vuex'
 
 Vue.config.productionTip = false
 
 Vue.use(appVue)
 Vue.use(VueProgressBar)
 
+const { store, router } = use(Vue, {
+  filters,
+  components,
+  VueRouter,
+  Vuex
+})
+
 export default Vue
 
 export {
-  components,
-  filters
+  store,
+  router
 }
--- example/main.js
+++ example/main.js
@@ -1,19 +1,9 @@
 import 'promise-polyfill/src/polyfill'
 import 'core-js/modules/es6.object.assign'
 import 'core-js/modules/es6.typed.float32-array'
-import Vue, { filters, components } from './config/vue'
-import use from 'vue-use'
-import VueRouter from './config/vue-router'
-import Vuex from './config/vuex'
+import Vue, { store, router } from './config/vue'
 import '../src/scss/app.scss'
 import './scss/app.scss'
-
-const { store, router } = use(Vue, {
-  filters,
-  components,
-  VueRouter,
-  Vuex
-})
 
 /* eslint-disable no-new */
 new Vue({
