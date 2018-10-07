ae9a4c75ac89dab0cae113f3d40f59fab773c55b
+v <ljw@live.jp>
2018-10-07 16:06:53 +0800

    release

--- example/components/social/extend/airasia/routes.js
+++ example/components/social/extend/airasia/routes.js
@@ -1,7 +1,5 @@
 module.exports = [
   /* [currency, departure, destination] */
-  ['cny', 'dmk', 'ckg'],
-  ['cny', 'kul', 'ckg'],
-  ['cny', 'dmk', 'mle'],
-  ['cny', 'kul', 'mle']
+  ['cny', 'ckg', 'mle'],
+  ['cny', 'mle', 'ckg']
 ]
