cc4b10642a446dff5a458d8de1006f78e22afd6b
+v <ljw@live.jp>
2018-10-13 21:57:39 +0800

    update: meta

--- example/config/html/meta.js
+++ example/config/html/meta.js
@@ -1,7 +1,10 @@
 module.exports = [
   '<meta charset="utf-8">',
-  '<meta name="viewport" content="initial-scale=1.0">',
-  '<meta name=renderer content=webkit>',
-  '<meta name=force-rendering content=webkit>',
+  '<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">',
+  '<meta name="renderer" content="webkit">',
+  '<meta name="force-rendering" content="webkit">',
+  '<meta name="applicable-device" content="mobile">',
+  '<meta http-equiv="Cache-Control" content="no-transform">',
+  '<meta http-equiv="Cache-Control" content="no-siteapp">',
   '<meta http-equiv=X-UA-Compatible content="IE=Edge, chrome=1">'
 ]
