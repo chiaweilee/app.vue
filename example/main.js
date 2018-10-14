1b1d151826c1d70a3e9313a6bb8d56d47796d754
+v <ljw@live.jp>
2018-10-14 21:35:58 +0800

    whatwg-fetch

--- example/main.js
+++ example/main.js
@@ -1,6 +1,7 @@
 import 'promise-polyfill/src/polyfill'
 import 'core-js/modules/es6.object.assign'
 import 'core-js/modules/es6.typed.float32-array'
+import 'whatwg-fetch'
 import Vue, { store, router } from './config/vue'
 import '../src/scss/app.scss'
 import './scss/app.scss'
